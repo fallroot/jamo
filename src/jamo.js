a88eab8b7c7188f22c67c8c6dd4e56fb43c87cad
CK Moon <fallroot@gmail.com>
2019-06-25 14:48:29 +0900

    `decompose`, `decomposeAsCompat` 함수에서 공통으로 사용하는 코드 분리

--- src/jamo.js
+++ src/jamo.js
@@ -3,23 +3,38 @@ const COMPAT_VOWELS = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', '
 const CHOSEONG_TO_COMPAT = [0, 1, 3, 6, 7, 8, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 const JONGSEONG_TO_COMPAT = [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29]
 
-function decompose (text) {
+function decomposeAsOffset (text) {
+  const offset = text.codePointAt(0) - 0xAC00
+  const jongseong = offset % 28
+  const jungseong = ((offset - jongseong) / 28) % 21
+  const choseong = (((offset - jongseong) / 28) - jungseong) / 21
+
+  return {
+    choseong,
+    jungseong,
+    jongseong
+  }
+}
+
+function decomposeWith (text, callback) {
   return Array.from(text).map(ch => {
     if (!isSyllable(ch)) {
       return [ch]
     }
 
-    const offset = ch.codePointAt(0) - 0xAC00
-    const jongseong = offset % 28
-    const jungseong = ((offset - jongseong) / 28) % 21
-    const choseong = (((offset - jongseong) / 28) - jungseong) / 21
+    return callback(decomposeAsOffset(ch))
+  })
+}
+
+function decompose (text) {
+  return decomposeWith(text, offsets => {
     const result = [
-      String.fromCodePoint(0x1100 + choseong),
-      String.fromCodePoint(0x1161 + jungseong)
+      String.fromCodePoint(0x1100 + offsets.choseong),
+      String.fromCodePoint(0x1161 + offsets.jungseong)
     ]
 
-    if (jongseong) {
-      result.push(String.fromCodePoint(0x11A8 + jongseong - 1))
+    if (offsets.jongseong) {
+      result.push(String.fromCodePoint(0x11A8 + offsets.jongseong - 1))
     }
 
     return result
@@ -27,22 +42,14 @@ function decompose (text) {
 }
 
 function decomposeAsCompat (text) {
-  return Array.from(text).map(ch => {
-    if (!isSyllable(ch)) {
-      return [ch]
-    }
-
-    const offset = ch.codePointAt(0) - 0xAC00
-    const jongseong = offset % 28
-    const jungseong = ((offset - jongseong) / 28) % 21
-    const choseong = (((offset - jongseong) / 28) - jungseong) / 21
+  return decomposeWith(text, offsets => {
     const result = [
-      COMPAT_CONSONANTS[CHOSEONG_TO_COMPAT[choseong]],
-      COMPAT_VOWELS[jungseong]
+      COMPAT_CONSONANTS[CHOSEONG_TO_COMPAT[offsets.choseong]],
+      COMPAT_VOWELS[offsets.jungseong]
     ]
 
-    if (jongseong) {
-      result.push(COMPAT_CONSONANTS[JONGSEONG_TO_COMPAT[jongseong - 1]])
+    if (offsets.jongseong) {
+      result.push(COMPAT_CONSONANTS[JONGSEONG_TO_COMPAT[offsets.jongseong - 1]])
     }
 
     return result
