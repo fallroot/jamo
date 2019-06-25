0ac8109ed0597ed0758cf1692b41656fac510d10
CK Moon <fallroot@gmail.com>
2019-06-25 12:08:44 +0900

    음절을 호환성 자모 단위로 분리하는 `decomposeAsCompat` 함수 작성

--- src/jamo.js
+++ src/jamo.js
@@ -1,3 +1,8 @@
+const COMPAT_CONSONANTS = ['ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+const COMPAT_VOWELS = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+const CHOSEONG_TO_COMPAT = [0, 1, 3, 6, 7, 8, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+const JONGSEONG_TO_COMPAT = [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29]
+
 function decompose (text) {
   return Array.from(text).map(ch => {
     if (!isSyllable(ch)) {
@@ -21,6 +26,29 @@ function decompose (text) {
   })
 }
 
+function decomposeAsCompat (text) {
+  return Array.from(text).map(ch => {
+    if (!isSyllable(ch)) {
+      return [ch]
+    }
+
+    const offset = ch.codePointAt(0) - 0xAC00
+    const jongseong = offset % 28
+    const jungseong = ((offset - jongseong) / 28) % 21
+    const choseong = (((offset - jongseong) / 28) - jungseong) / 21
+    const result = [
+      COMPAT_CONSONANTS[CHOSEONG_TO_COMPAT[choseong]],
+      COMPAT_VOWELS[jungseong]
+    ]
+
+    if (jongseong) {
+      result.push(COMPAT_CONSONANTS[JONGSEONG_TO_COMPAT[jongseong - 1]])
+    }
+
+    return result
+  })
+}
+
 function isSyllable (text) {
   return Array.from(text).every(ch => {
     const codePoint = ch.codePointAt(0)
@@ -31,5 +59,6 @@ function isSyllable (text) {
 
 export default {
   decompose,
+  decomposeAsCompat,
   isSyllable
 }
--- test/jamo.test.js
+++ test/jamo.test.js
@@ -25,3 +25,20 @@ describe('decompose 함수는 한글 음절을 자모 단위로 분해한다', (
     expect(jamo.decompose('히')).toEqual([['ᄒ', 'ᅵ']])
   })
 })
+
+describe('decomposeAsCompat 함수는 한글 음절을 호환성 자모 단위로 분해한다', () => {
+  test('한글 음절이 아닌 문자는 그대로 돌려준다', () => {
+    expect(jamo.decomposeAsCompat('abc')).toEqual([['a'], ['b'], ['c']])
+    expect(jamo.decomposeAsCompat('ㄱㄴㄷ')).toEqual([['ㄱ'], ['ㄴ'], ['ㄷ']])
+  })
+
+  test('받침이 있는 음절을 초/중/종성 순서의 배열로 돌려준다', () => {
+    expect(jamo.decomposeAsCompat('각')).toEqual([['ㄱ', 'ㅏ', 'ㄱ']])
+    expect(jamo.decomposeAsCompat('힣')).toEqual([['ㅎ', 'ㅣ', 'ㅎ']])
+  })
+
+  test('받침이 없는 음절을 초/중성 순서의 배열로 돌려준다', () => {
+    expect(jamo.decomposeAsCompat('가')).toEqual([['ㄱ', 'ㅏ']])
+    expect(jamo.decomposeAsCompat('히')).toEqual([['ㅎ', 'ㅣ']])
+  })
+})
